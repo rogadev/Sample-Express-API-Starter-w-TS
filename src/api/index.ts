10d749ca477b7b0521c7827567ec7f69497bf221
Roga <ryanroga@gmail.com>
2023-03-01 13:43:30 -0800

    Preparing for our API's

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,13 @@
 import express from 'express';
 
 import MessageResponse from '../interfaces/MessageResponse';
-import emojis from './emojis';
 
 const router = express.Router();
 
 router.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
-    message: 'API - 👋🌎🌍🌏',
+    message: 'Welcome to the API.',
   });
 });
-
-router.use('/emojis', emojis);
 
 export default router;
