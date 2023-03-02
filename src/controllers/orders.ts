0559566bf89df6ba948926cb9a846d2820f37ab1
Roga <ryanroga@gmail.com>
2023-03-01 16:07:10 -0800

    Stubbed out Orders Controller

---
+++ src/controllers/orders.ts
@@ -0,0 +1,47 @@
+export const getAll = (req, res) => {
+  // TODO - get all orders.
+  // TODO - page should populate form inputs with form data. Form data is sent if we are creating a new order, submit, and then get an error.
+  // TODO - page should populate form errors with error data. Form data is sent if we are editing an order, submit, and then get an error.
+  // TODO - clicking on an order in the list will take us to the order detail page for that order.
+  res.send('GET /orders');
+};
+
+export const createOne = (req, res) => {
+  // TODO - get form data from request body.
+  // TODO - validate form data.
+  // TODO - if there's an error:
+  // TODO  - returns data for a form to create a new order.
+  // TODO  - returns error data related to each field of the form.
+  // TODO - if no errors, create a new order.
+  res.send('POST /orders');
+};
+
+export const getOne = (req, res) => {
+  const { id } = req.params;
+  // TODO - get order by id.
+  // TODO - if order doesn't exist, return 404.
+  // TODO - if order exists, return order data.
+  res.send(`GET /orders/${id}`);
+};
+
+export const updateOne = (req, res) => {
+  const { id } = req.params;
+  // TODO - find this order matching the id.
+  // TODO - if order doesn't exist, return 404.
+  // TODO - if order exists:
+  // TODO   - get form data from request body.
+  // TODO   - validate form data.
+  // TODO   - if there's an error:
+  // TODO    - returns data for a form to edit an order.
+  // TODO    - returns error data related to each field of the form.
+  // TODO   - if no errors, update order.
+  res.send(`PUT /orders/${id}`);
+};
+
+export const deleteOne = (req, res) => {
+  const { id } = req.params;
+  // TODO - find this order matching the id.
+  // TODO - if order doesn't exist, return 404.
+  // TODO - if order exists, delete order.
+  res.send(`DELETE /orders/${id}`);
+};
