e7e0ea6dcf9173092312d9aeb1c51527591d7e1c
Roga <ryanroga@gmail.com>
2023-03-01 16:31:33 -0800

    Stubbed out the scripts for orders.js
    This file will handle form submission, population, updating, errors, etc.

---
+++ src/public/scripts/orders.js
@@ -0,0 +1,5 @@
+// TODO - generate the form HTML
+// TODO - populate the form with order data, if provided
+// TODO - populate form errors with error data, if provided
+// TODO - add event listener to the form to handle submitting the data (send to api route and wait for response)
+// TODO - update form data and errors based on response
