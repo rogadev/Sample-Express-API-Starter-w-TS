14c3b3641521df397e021446a471769ef2901bad
Roga <ryanroga@gmail.com>
2023-03-01 16:27:24 -0800

    Updated Tests (passing)

--- test/api.test.ts
+++ test/api.test.ts
@@ -9,17 +9,7 @@ describe('GET /api/v1', () => {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, {
-        message: 'API - 👋🌎🌍🌏',
+        message: 'Welcome to the API.',
       }, done);
-  });
-});
-
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', (done) => {
-    request(app)
-      .get('/api/v1/emojis')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, ['😀', '😳', '🙄'], done);
   });
 });
--- test/app.test.ts
+++ test/app.test.ts
@@ -13,13 +13,11 @@ describe('app', () => {
 });
 
 describe('GET /', () => {
-  it('responds with a json message', (done) => {
+  it("responds with a list of anchor tags that describe each of the routes we're building.", (done) => {
     request(app)
       .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-      }, done);
+      .set('Accept', 'text/html')
+      .expect('Content-Type', /html/)
+      .expect(200, done);
   });
 });
